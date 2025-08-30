3bddf7393a1848519ec5916d672d00ae7482d3f2
elCodigoDeMarmota <margarita.loretos@gmail.com>
2025-08-30 01:18:24 -0400

    Script SQL

---
+++ Scripts/init.sql
@@ -0,0 +1,22 @@
+﻿
+CREATE TABLE dbo.Snippets (
+    Id         NVARCHAR(450) NOT NULL PRIMARY KEY,
+    Code       NVARCHAR(MAX) NOT NULL,
+    Language   NVARCHAR(MAX) NOT NULL,
+    CreatedAt  DATETIME2(7)  NOT NULL
+);
+GO
+
+
+CREATE PROCEDURE dbo.BuscarSnippet
+    @Id NVARCHAR(64)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT Code
+    FROM dbo.Snippets
+    WHERE Id = @Id;
+END
+GO
+
